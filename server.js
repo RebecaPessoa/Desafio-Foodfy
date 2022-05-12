2e17b64eee736874f8901bfb02caa7a2f6e392d4
RebecaPessoa <rebecahayanny17@gmail.com>
2022-05-12 20:40:44 -0300

    organização de diretórios

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+  express:server
+})
+
+
+
 server.get("/", function(req, res){
-  return res.send("Hi!")
+  return res.render("index")
 })
 
 server.listen(5000, function() {
